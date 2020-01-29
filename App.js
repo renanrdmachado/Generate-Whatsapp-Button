f1a652980ee5b51a22a56696e2dedd57882cd409
Renan Machado <renan@rdmachado.com.br>
2020-01-29 17:38:14 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GenerateLink from './Components/Generate_link';
+
+function App() {
+  return (
+    <div className="App">
+      <GenerateLink />
+    </div>
+  );
+}
+
+export default App;
